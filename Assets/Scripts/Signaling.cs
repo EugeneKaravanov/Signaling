66a3a1938ad3437d8d702a24007af396b234649e
EugeneKaravanov <94747745+EugeneKaravanov@users.noreply.github.com>
2023-05-29 15:55:30 +0300

    Commit 3

--- Assets/Scripts/Signaling.cs
+++ Assets/Scripts/Signaling.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class Signaling : MonoBehaviour
 {
-    private AudioSource _audioSource;
-    private float _volumeChange = 0.1f;
-    private float _maxVolume = 1;
-    private float _minVolume = 0;
-    private bool _isThiefInHouse = false;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Update is called once per frame
+    void Update()
     {
-        _isThiefInHouse = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        _isThiefInHouse = false;
-    }
-
-    private void Update()
-    {
-        if(_audioSource.volume < _maxVolume && _isThiefInHouse)
-        {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, _volumeChange * Time.deltaTime);
-        }
-        else
-        {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _minVolume, _volumeChange * Time.deltaTime);
-        }
+        
     }
 }
